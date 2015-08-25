f0d3334abce0d866ae2b9e3ab55c587c70e19144
Symphorien Gibol <symphorien.gibol@gmail.com>
2015-08-25 22:11:50 +0200

    No validator on User.phone_number since this field will never be written by users.

--- platalbank_auth/models.py
+++ platalbank_auth/models.py
@@ -3,16 +3,7 @@
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core import validators
 
 class User(AbstractUser):
-    phone_number = models.CharField(
-            max_length=14,
-            validators=[
-                validators.RegexValidator(r'^\d\d-$\d\d-$\d\d-$\d\d-$\d\d$',
-                    'Enter a valid phone number : format : 06-66-66-66-66',
-                    'invalid')
-                ]
-            )
-
+    phone_number = models.CharField(max_length=32)
     room = models.CharField(max_length=128)
