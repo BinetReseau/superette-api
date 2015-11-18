74777b46a9d23c590d7e22c60f8e3e8b186eb42f
Léo Gaspard <leo@gaspard.io>
2015-11-18 03:23:08 +0100

    Use descriptive names for limits in Account model

--- platalbank_core/models/account.py
+++ platalbank_core/models/account.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Account(models.Model):
-    balance = models.BigIntegerField() # Number of cents owned
-    description = models.CharField(max_length=1024)
-    short_name = models.CharField(max_length=128)
+    DESCRIPTION_MAX_LENGTH = 1024
+    SHORT_NAME_MAX_LENGTH = 128
+
+    balance       = models.BigIntegerField() # Number of cents owned
+    description   = models.CharField(max_length = DESCRIPTION_MAX_LENGTH)
+    short_name    = models.CharField(max_length = SHORT_NAME_MAX_LENGTH)
     # TODO: owner = models.ForeignKey("LegalPerson")
 
     def __str__(self):
