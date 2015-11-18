516f3362c855051a809e7897f79551279f7979c8
Léo Gaspard <leo@gaspard.io>
2015-11-18 03:44:12 +0100

    Use factory_boy for testing purposes

--- platalbank_core/tests/test_transactions.py
+++ platalbank_core/tests/test_transactions.py
@@ -1,46 +1,18 @@
-from platalbank_core.models import Event, Transaction, Account
+from platalbank_core.factories import TransactionFactory
 
 from rest_framework.test import APITestCase
 
-def reload(obj):
-    return obj.__class__.objects.get(pk=obj.pk)
-
 class TransactionTests(APITestCase):
-    @classmethod
-    def setUpTestData(self):
-        super(TransactionTests, self).setUpTestData()
-
-        self.event, _ = Event.objects.get_or_create(label='Test', through_khube=True, writable=True)
-        self.accA, _ = Account.objects.get_or_create(short_name='A', balance=4200)
-        self.accB, _ = Account.objects.get_or_create(short_name='B', balance=1337)
-
     def test_transaction(self):
-        self.accA, self.accB = reload(self.accA), reload(self.accB)
-        self.assertEqual(self.accA.balance, 4200)
-        self.assertEqual(self.accB.balance, 1337)
-        Transaction.objects.create(
-            amount=500,
-            label='Test',
-            debited_account=self.accA,
-            credited_account=self.accB,
-            event=self.event
+        t = TransactionFactory.create(
+            amount                    = 500,
+            debited_account__balance  = 4200,
+            credited_account__balance = 1337
         )
-        self.accA, self.accB = reload(self.accA), reload(self.accB)
-        self.assertEqual(self.accA.balance, 3700)
-        self.assertEqual(self.accB.balance, 1837)
+        self.assertEqual(t.debited_account.balance, 3700)
+        self.assertEqual(t.credited_account.balance, 1837)
 
-    # Test what happens when save is called twice
-    def test_not_duplicate_transfer(self):
-        self.accA, self.accB = reload(self.accA), reload(self.accB)
-        self.assertEqual(self.accA.balance, 4200)
-        self.assertEqual(self.accB.balance, 1337)
-        Transaction.objects.create(
-            amount=500,
-            label='Test',
-            debited_account=self.accA,
-            credited_account=self.accB,
-            event=self.event
-        ).save()
-        self.accA, self.accB = reload(self.accA), reload(self.accB)
-        self.assertEqual(self.accA.balance, 3700)
-        self.assertEqual(self.accB.balance, 1837)
+        # Test what happens when save is called twice
+        t.save()
+        self.assertEqual(t.debited_account.balance, 3700)
+        self.assertEqual(t.credited_account.balance, 1837)
