BinetReseau/PlatalBank

Add datetime field to transaction model

platalbank_core/models/transaction.py
@@ -24,6 +24,8 @@ class Transaction(models.Model):
     state = models.CharField(max_length=1, choices=_STATE_CHOICES,default=PENDING)
     amount = models.BigIntegerField() # Number of cents transferred
     label = models.CharField(max_length=1024)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     debited_account = models.ForeignKey("Account", related_name="out_transactions")
     credited_account = models.ForeignKey("Account", related_name="in_transactions")

platalbank_core/serializers/transaction.py
@@ -5,4 +5,4 @@ from platalbank_core.models import Transaction
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Transaction
-        fields = ('url', 'id', 'state', 'amount', 'label', 'debited_account', 'credited_account', 'event')
+        fields = ('url', 'id', 'state', 'amount', 'label', 'debited_account', 'credited_account', 'event','timestamp','last_modified')

BinetReseau/superette-api

platalbank_core/models/transaction.py
@@ -24,6 +24,8 @@ class Transaction(models.Model):
     state = models.CharField(max_length=1, choices=_STATE_CHOICES,default=PENDING)
     amount = models.BigIntegerField() # Number of cents transferred
     label = models.CharField(max_length=1024)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     debited_account = models.ForeignKey("Account", related_name="out_transactions")
     credited_account = models.ForeignKey("Account", related_name="in_transactions")

platalbank_core/serializers/transaction.py
@@ -5,4 +5,4 @@ from platalbank_core.models import Transaction
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Transaction
-        fields = ('url', 'id', 'state', 'amount', 'label', 'debited_account', 'credited_account', 'event')
+        fields = ('url', 'id', 'state', 'amount', 'label', 'debited_account', 'credited_account', 'event','created','last_modified')