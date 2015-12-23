285aa006149ebefab9e012744ee548d970a26bbd
Varal7 <varaal@gmail.com>
2015-12-23 18:23:17 +0100

    Changed name from Short to Flat

--- platalbank_core/serializers/transaction.py
+++ platalbank_core/serializers/transaction.py
@@ -13,7 +13,7 @@ class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     credited_account = ShortAccountSerializer()
     event            = ShortEventSerializer()
 
-class ShortTransactionSerializer(serializers.HyperlinkedModelSerializer):
+class FlatTransactionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model  = Transaction
         fields = ('url', 'id', 'state', 'amount', 'label', 'debited_account', 'credited_account', 'event', 'created','last_modified')
--- platalbank_core/views/transaction.py
+++ platalbank_core/views/transaction.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
 
 from platalbank_core.models import Transaction
-from platalbank_core.serializers import TransactionSerializer, ShortTransactionSerializer
+from platalbank_core.serializers import TransactionSerializer, FlatTransactionSerializer
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'destroy']:
-            return ShortTransactionSerializer
+            return FlatTransactionSerializer
         return TransactionSerializer
