064d58d93b8e94975ad31c833b758fc77cdef079
Varal7 <varaal@gmail.com>
2016-02-21 22:12:59 +0100

    Added filtering for transactions

--- platalbank_core/views/transaction.py
+++ platalbank_core/views/transaction.py
@@ -5,6 +5,7 @@ from platalbank_core.serializers import TransactionSerializer, FlatTransactionSe
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
+    filter_fields = ('state', 'event', 'credited_account', 'debited_account',)
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'destroy']:
