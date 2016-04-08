9a5f140f4464127fc7a3bfb399c2d334809fe724
Varal7 <varaal@gmail.com>
2016-04-09 00:39:18 +0200

    Add cancel method for transaction

--- platalbank_core/views/transaction.py
+++ platalbank_core/views/transaction.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, decorators
 
 from platalbank_core.models import Transaction
 from platalbank_core.serializers import TransactionSerializer, FlatTransactionSerializer
+from rest_framework.response import Response
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
@@ -11,3 +12,23 @@ class TransactionViewSet(viewsets.ModelViewSet):
         if self.action in ['create', 'update', 'destroy']:
             return FlatTransactionSerializer
         return TransactionSerializer
+
+    @decorators.detail_route()
+    def cancel(self, request, pk=None):
+        try:
+            t = Transaction.objects.get(pk=pk)
+            if t.state != Transaction.ABORTED:
+                t.state = Transaction.ABORTED
+                if t.debited_account.pk != t.credited_account.pk:
+                    t.credited_account.balance -= t.amount
+                    t.debited_account.balance += t.amount
+                    t.credited_account.save()
+                    t.debited_account.save()
+                print(t.state)
+                t.save()
+                return Response(204)
+            else:
+                return Response("Already canceled", 400)
+        except Exception as e:
+            print (e.message)
+            return Response(400)
